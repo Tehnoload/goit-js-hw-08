907b9fb7025add62bce1f9b24f4cd177703ae146
Tehnoload <11tehnoload11@gmail.com>
2023-08-10 22:05:27 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,10 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const feedbackFormData = {};
+const feedbackFormData = {
+  message: '',
+  email: '',
+};
 
 const refs = {
   form: document.querySelector('form'),
@@ -15,25 +18,31 @@ refs.form.addEventListener(
   'input',
   throttle(event => {
     feedbackFormData[event.target.name] = event.target.value;
+
     localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackFormData));
   }, 500)
 );
 
 populateTextarea();
 
-function onInputSubmit(evt) {
-  evt.preventDefault();
-  console.log(feedbackFormData);
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+function populateTextarea() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    if (savedData.message !== undefined) {
+      refs.textarea.value = savedData.message;
+      feedbackFormData.message = savedData.message;
+    }
+    if (savedData.email !== undefined) {
+      refs.input.value = savedData.email;
+      feedbackFormData.email = savedData.email;
+    }
+  }
 }
 
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  const parseMessage = JSON.parse(savedMessage);
-
-  if (savedMessage) {
-    refs.textarea.value = parseMessage.message;
-    refs.input.value = parseMessage.email;
-  }
+function onInputSubmit(evt) {
+  evt.preventDefault();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
